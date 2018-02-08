7757ff2dd486bafc09678ecb285acfaf3251e31b
mirelatv <noelia.mirela@gmail.com>
2018-02-07 21:51:27 -0500

    subiendo maquetado de vista 2 , css y js

---
+++ js/collage.js
@@ -0,0 +1,34 @@
+let begin = () => {
+
+document.addEventListener('dragstart',drag);
+document.addEventListener('drop',drop);
+document.addEventListener('dragover',allowDrop);
+
+
+//ondragover :funcion 1 previniendo el comportamiento de arrastar un elemento que  no es una  caracteristica comun
+function allowDrop(ev){ 
+    ev.preventDefault();
+    
+}
+
+// ondragstar:function 2 obteniendo o arrastrando el elemento 
+function drag(ev){
+    
+    ev.dataTransfer.setData('text',ev.target.id);
+}
+
+//ondrop:fucntion 3 soltando el elemento
+
+function drop(ev){
+    ev.preventDefault();
+    //ev.dataTransfer.dropEffect = 'copy';
+
+    var data = ev.dataTransfer.getData('text');
+    
+    ev.target.appendChild(document.getElementById(data));
+
+}
+
+};
+
+window.addEventListener('load',begin);
